4ba7c35aeeb5074c4d121279be40f44147cd2ff1
Felix Fennell <felix@felixfennell.co.uk>
2019-03-04 09:55:22 +0000

    Fixing running unit tests in PyCharm
    Previous app.py structure meant an application was created on import in the tests which blocked PyCharm from running the actual tests.
    
    We now only create an application if the module is ran directly.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+* Refactored `app.py` to prevent creating an application on import
+
 ## [0.1.1] - 2019-03-02
 
 ### Fixed
--- app.py
+++ app.py
@@ -7,34 +7,33 @@ from flask_request_id_header.middleware import RequestID
 
 
 def create_app_with_middleware():
-    _app = Flask(__name__)
+    app = Flask(__name__)
 
     # Configure and load Middleware
-    _app.config['REQUEST_ID_UNIQUE_VALUE_PREFIX'] = 'TEST-'
-    RequestID(_app)
+    app.config['REQUEST_ID_UNIQUE_VALUE_PREFIX'] = 'TEST-'
+    RequestID(app)
 
-    @_app.route("/sample")
+    # Support running integration tests
+    @app.cli.command()
+    def test():
+        """Run integration tests."""
+        tests = unittest.TestLoader().discover(os.path.join(os.path.dirname(__file__), 'tests'))
+        unittest.TextTestRunner(verbosity=2).run(tests)
+
+    @app.route("/sample")
     def sample():
         return '', HTTPStatus.NO_CONTENT
 
-    return _app
+    return app
 
 
-app = create_app_with_middleware()
+if __name__ == "__main__":
+    test_app = create_app_with_middleware()
 
-
-# Support PyCharm debugging
-if 'PYCHARM_HOSTED' in os.environ:
-    # Exempting Bandit security issue (binding to all network interfaces)
-    #
-    # All interfaces option used because the network available within the container can vary across providers
-    # This is only used when debugging with PyCharm. A standalone web server is used in production.
-    app.run(host='0.0.0.0', port=9000, debug=True, use_debugger=False, use_reloader=False)  # nosec
-
-
-# Support running integration tests
-@app.cli.command()
-def test():
-    """Run integration tests."""
-    tests = unittest.TestLoader().discover(os.path.join(os.path.dirname(__file__), 'tests'))
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    # Support PyCharm debugging
+    if 'PYCHARM_HOSTED' in os.environ:
+        # Exempting Bandit security issue (binding to all network interfaces)
+        #
+        # All interfaces option used because the network available within the container can vary across providers
+        # This is only used when debugging with PyCharm. A standalone web server is used in production.
+        test_app.run(host='0.0.0.0', port=9000, debug=True, use_debugger=False, use_reloader=False)  # nosec
